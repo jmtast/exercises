cb2144a954740f91e01246ac02c12783acce8ceb
Juan Manuel Tastzian <jm@tast.com.ar>
2014-03-06 13:26:29 -0300

    Adds test to ensure Permission parameters class

--- lawyers/model/permission.rb
+++ lawyers/model/permission.rb
@@ -2,7 +2,7 @@
 
 class Permission
   def initialize (source, destination, case_document)
-    if source.instance_of? Lawyer && destination.instance_of? Lawyer
+    if (source.instance_of?(Lawyer) && destination.instance_of?(Lawyer))
       if (case_document.get_owner == source)
         @source = source
         @destination = destination
